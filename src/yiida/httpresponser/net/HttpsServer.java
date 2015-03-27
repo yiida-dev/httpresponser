eba73bfaed96342bdcab9489fab72fb7cd0533be
yiida <yiida-dev@gmail.com>
2015-03-27 14:13:49 +0900

    comment out static system properties.

--- src/yiida/httpresponser/net/HttpsServer.java
+++ src/yiida/httpresponser/net/HttpsServer.java
@@ -4,13 +4,16 @@ import javax.net.ServerSocketFactory;
 import javax.net.ssl.SSLServerSocketFactory;
 
 public class HttpsServer extends HttpServer {
+    
+    /* comment in and modify values if you want to fix SSL environment.
     static {
         System.setProperty("javax.net.ssl.keyStore",
                 "/path/your_keystore");
         System.setProperty("javax.net.ssl.keyStoreType", "JKS");
         System.setProperty("javax.net.ssl.keyStorePassword", "password");
     }
-
+    */
+    
     public HttpsServer() {}
 
     public HttpsServer(int connectionCount, int port) {
